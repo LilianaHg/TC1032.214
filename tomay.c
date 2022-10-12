e92b082d0b5c04ad6a187f5d5768c2c6189b31f6
LilianaHg <A01640873@tec.mx>
2022-10-12 13:55:55 +0000

    vocales

---
+++ tomay.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+
+char c;
+char may;
+
+int n;
+
+int main(){
+    
+
+    do{
+        char vocal = '1';
+       // n = read(STDIN_FILENO,&c,1); //leer la entrada estándar, guardar en c y leer uno por una
+        //may = toupper(c); //Convertir a mayúsculas
+        //write(STDOUT_FILENO,&may,1);
+
+        n = read(STDIN_FILENO,&c,1);
+        may = toupper(c); //Convertir a mayúsculas
+        if (may == 'A' || may =='E' || may=='I' || may=='O' || may=='U' ){
+        write(STDOUT_FILENO,&vocal,1);
+    }
+        
+
+    }
+    while(n!=0);
+    return 0;
+}
