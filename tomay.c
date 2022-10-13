1507cbc7b1c359147fd764cadd28b206881ee6b7
LilianaHg <A01640873@tec.mx>
2022-10-13 20:08:56 +0000

    Practica 4

--- tomay.c
+++ tomay.c
@@ -12,18 +12,19 @@ int main(){
 
     do{
         char vocal = '1';
-       // n = read(STDIN_FILENO,&c,1); //leer la entrada estándar, guardar en c y leer uno por una
-        //may = toupper(c); //Convertir a mayúsculas
-        //write(STDOUT_FILENO,&may,1);
+       //n = read(STDIN_FILENO,&c,1); //leer la entrada estándar, guardar en c y leer uno por una
+       //may = toupper(c); //Convertir a mayúsculas
+       //write(STDOUT_FILENO,&may,1);
 
         n = read(STDIN_FILENO,&c,1);
         may = toupper(c); //Convertir a mayúsculas
         if (may == 'A' || may =='E' || may=='I' || may=='O' || may=='U' ){
         write(STDOUT_FILENO,&vocal,1);
     }
-        
-
     }
     while(n!=0);
     return 0;
+        
+
 }
+   
